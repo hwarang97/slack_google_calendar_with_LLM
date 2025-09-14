6bb31c5c5421d1218c60ba3e5bcdab8602163c99
dolggul2 <dolggul2@gmail.com>
2025-09-14 11:38:52 +0900

    style: appley pre-commit auto-formatting

--- ngrok_server.py
+++ ngrok_server.py
@@ -1,19 +1,19 @@
 import os
-from pyngrok import ngrok
-from pyngrok import conf
+
 from dotenv import load_dotenv
+from pyngrok import conf, ngrok
 
 # setttings
 load_dotenv()
-conf.get_default().ngrok_path = './ngrok.exe'
-ngrok.set_auth_token(os.getenv('NGROK_AUTHTOKEN'))
+conf.get_default().ngrok_path = "./ngrok.exe"
+ngrok.set_auth_token(os.getenv("NGROK_AUTHTOKEN"))
 PORT = 8000
 
-# run ngrok 
+# run ngrok
 tunnel = ngrok.connect(PORT)
 
 process = ngrok.get_ngrok_process()
-print(f'public IP: {tunnel}')
+print(f"public IP: {tunnel}")
 
 try:
     process.proc.wait()
