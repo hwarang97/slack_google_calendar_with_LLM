2d40b570a8fc52b7b3bba93bb338a81d754ed17e
dolggul2 <dolggul2@gmail.com>
2025-09-14 11:02:51 +0900

    feat: implement ngrok tunneling for slack webhook testing
    This creates a public URL that can be registed with Slack, allowing webhook events to be recieved and tested locally during development.

---
+++ ngrok_server.py
@@ -0,0 +1,22 @@
+import os
+from pyngrok import ngrok
+from pyngrok import conf
+from dotenv import load_dotenv
+
+# setttings
+load_dotenv()
+conf.get_default().ngrok_path = './ngrok.exe'
+ngrok.set_auth_token(os.getenv('NGROK_AUTHTOKEN'))
+PORT = 8000
+
+# run ngrok 
+tunnel = ngrok.connect(PORT)
+
+process = ngrok.get_ngrok_process()
+print(f'public IP: {tunnel}')
+
+try:
+    process.proc.wait()
+except KeyboardInterrupt:
+    print(" Shutting down server.")
+    ngrok.kill()
