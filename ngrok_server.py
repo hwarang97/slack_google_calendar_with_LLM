67ae1935387d116850bb8117aa79ac6427222e2c
dolggul2 <dolggul2@gmail.com>
2025-09-26 19:37:31 +0900

    fix: resolve none type error for NGROK_AUTHTOKEN

--- ngrok_server.py
+++ ngrok_server.py
@@ -1,22 +1,37 @@
+import logging
 import os
+import sys
 
 from dotenv import load_dotenv
 from pyngrok import conf, ngrok
 
-# setttings
+# loggin settings
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="ngrok_server.log",
+    filemode="w",
+)
+
+# ngrok setttings
 load_dotenv()
+auth_token = os.getenv("NGROK_AUTHTOKEN")
+if auth_token:
+    ngrok.set_auth_token(auth_token)
+    logging.info("NGROK_AUTHTOKEN has been set seccuessfully.")
+else:
+    logging.info("Error: NGROK_AUTHTOKEN enveironment variable is not set.")
+    sys.exit(1)
+
 conf.get_default().ngrok_path = "./ngrok.exe"
-ngrok.set_auth_token(os.getenv("NGROK_AUTHTOKEN"))
 PORT = 8000
 
 # run ngrok
 tunnel = ngrok.connect(PORT)
-
 process = ngrok.get_ngrok_process()
 print(f"public IP: {tunnel}")
 
 try:
     process.proc.wait()
 except KeyboardInterrupt:
-    print(" Shutting down server.")
+    logging.info("Shutting down server by user request (Ctrl+ c).")
     ngrok.kill()
