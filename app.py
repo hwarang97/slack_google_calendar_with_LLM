8cf3853655a4f281a7919ae40107bb5cde50f6cb
dolggul2 <dolggul2@gmail.com>
2025-09-28 11:55:49 +0900

    feat: implement endpoint to handle gcl slash command

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.post("/slack/command/gcl")
+async def handle_google_calendar_command():  # naming
+    return "It works"
+
+
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True)
