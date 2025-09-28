aec3237c6bdebd271aecd2b5ef9098a6f2a4c32b
dolggul2 <dolggul2@gmail.com>
2025-09-28 11:47:13 +0900

    feat: fix url by specifying ngrok domain
    - Specify domain for ngrok tunnel
    - Configure to ignore ngrok_server.log

--- ngrok_server.py
+++ ngrok_server.py
@@ -15,6 +15,8 @@ logging.basicConfig(
 # ngrok setttings
 load_dotenv()
 auth_token = os.getenv("NGROK_AUTHTOKEN")
+domain = os.getenv("NGROK_DOMAIN")
+
 if auth_token:
     ngrok.set_auth_token(auth_token)
     logging.info("NGROK_AUTHTOKEN has been set seccuessfully.")
@@ -22,11 +24,17 @@ else:
     logging.info("Error: NGROK_AUTHTOKEN enveironment variable is not set.")
     sys.exit(1)
 
+if domain:
+    logging.info("NGROK DOMAIN has benn set successfully.")
+else:
+    logging.info("Error: NGROK_DOMAIN enveironment variable is not set.")
+    sys.exit(1)
+
 conf.get_default().ngrok_path = "./ngrok.exe"
 PORT = 8000
 
 # run ngrok
-tunnel = ngrok.connect(addr=f"{PORT}")
+tunnel = ngrok.connect(addr=f"{PORT}", domain=f"{domain}")
 process = ngrok.get_ngrok_process()
 print(f"public IP: {tunnel}")
 
